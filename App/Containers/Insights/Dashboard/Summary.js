37631381615eef149e195c8e21df18bc7947c531
lavZoxima <lav.agarwal@zoxima.com>
2020-08-23 15:54:24 +0530

    Insights_Dasdboard_Summary_&_LeadAlerts_Date_Fix

--- App/Containers/Insights/Dashboard/Summary.js
+++ App/Containers/Insights/Dashboard/Summary.js
@@ -35,11 +35,12 @@ class DashboardSummaryScreen extends React.Component {
   render() {
     const {
 			loader,
-			data
+      data,
+      currentScreen
 		  } = this.props;
 
     
-  
+   
 
     return (
       	<ScrollView style={Styles.container}>
@@ -48,43 +49,43 @@ class DashboardSummaryScreen extends React.Component {
                 style={{ width: '88%', elevation: 0 }}
             
 	              content={[
-		                <GenericDisplayCardStrip key={'Total Walk Ins'} label={'Total Walk Ins'} value={'27'}/>,
+		                <GenericDisplayCardStrip key={'Total Walk Ins'} label={'Total Walk Ins'} value={data&&data.currentMonth&&data.currentMonth[8] ?  data.currentMonth[8].count : 0}/>,
 		                <Separator key={1}/>,
-		                <GenericDisplayCardStrip key={'Open Leads'} label={'Open Leads'} value={'15'}/>,
+		                <GenericDisplayCardStrip key={'Open Leads'} label={'Open Leads'} value={data&&data.currentMonth&&data.currentMonth[4] ?  data.currentMonth[4].count : 0}/>,
 		                <Separator key={2}/>,
-		                <GenericDisplayCardStrip key={'Won Leads'} label={'Won Leads'} value={'8'}/>,
+		                <GenericDisplayCardStrip key={'Won Leads'} label={'Won Leads'} value={data&&data.currentMonth&&data.currentMonth[1] ?  data.currentMonth[1].count : 0}/>,
 		                <Separator key={3}/>,
-		                <GenericDisplayCardStrip key={'Lost Leads'} label={'Lost Leads'} value={'11'}/>,
+		                <GenericDisplayCardStrip key={'Lost Leads'} label={'Lost Leads'} value={data&&data.currentMonth&&data.currentMonth[2] ?  data.currentMonth[2].count : 0}/>,
 		                <Separator key={4}/>,
-		                <GenericDisplayCardStrip key={'Ho Assigned Leads'} label={'Ho Assigned Leads'} value={'4'}/>
+		                <GenericDisplayCardStrip key={'Ho Assigned Leads'} label={'Ho Assigned Leads'} value={data&&data.currentMonth&&data.currentMonth[5] ?  data.currentMonth[5].count : 0}/>,
               		]}
             	/>
           	<HeadingBox value={'Previous Month'}/>
           		<GenericDisplayCard dark={false}
 	              style={{ width: '88%', elevation: 0 }}
 	              content={[
-		                <GenericDisplayCardStrip key={'Total Walk Insdwq'} label={'Total Walk Ins'} value={'50'}/>,
+		                <GenericDisplayCardStrip key={'Total Walk Insdwq'} label={'Total Walk Ins'} value={data&&data.prevMonth&&data.prevMonth[4] ?  data.prevMonth[4].count : 0}/>,
 		                <Separator key={1}/>,
-		                <GenericDisplayCardStrip key={'Open Leadswqd'} label={'Open Leads'} value={'15'}/>,
+		                <GenericDisplayCardStrip key={'Open Leadswqd'} label={'Open Leads'} value={data&&data.prevMonth&&data.prevMonth[1] ?  data.prevMonth[1].count : 0}/>,
 		                <Separator key={2}/>,
-		                <GenericDisplayCardStrip key={'Won Leadswqdq'} label={'Won Leads'} value={'10'}/>,
+		                <GenericDisplayCardStrip key={'Won Leadswqdq'} label={'Won Leads'} value={data&&data.prevMonth&&data.prevMonth[2] ?  data.prevMonth[2].count : 0}/>,
 		                <Separator key={3}/>,
-		                <GenericDisplayCardStrip key={'Lost Leadsqwd'} label={'Lost Leads'} value={'11'}/>,
+		                <GenericDisplayCardStrip key={'Lost Leadsqwd'} label={'Lost Leads'} value={data&&data.prevMonth&&data.prevMonth[0] ?  data.prevMonth[0].count : 0}/>,
 		                <Separator key={4}/>,
-		                <GenericDisplayCardStrip key={'Ho Assigned Leadswd'} label={'Ho Assigned Leads'} value={'4'}/>
+		                <GenericDisplayCardStrip key={'Ho Assigned Leadswd'} label={'Ho Assigned Leads'} value={data&&data.prevMonth&&data.prevMonth[3] ?  data.prevMonth[3].count : 0}/>
               		]}
             	/>
           	<HeadingBox value={'Product Performance \n(This Month)'}/>
           		<GenericDisplayCard dark={false}
 	              style={{ width: '88%', elevation: 0 }}
 	              content={[
-		                <GenericDisplayCardStrip key={'Optima LI'} label={'Optima LI'} value={'6'}/>,
+		                <GenericDisplayCardStrip key={'Optima LI'} label={'Optima LI'} value={data&&data.product&&data.product[0] ?  data.product[0].count : 0}/>,
 		                <Separator key={12}/>,
-		                <GenericDisplayCardStrip key={'Optima LA'} label={'Optima LA'} value={'3'}/>,
+		                <GenericDisplayCardStrip key={'Optima LA'} label={'Optima LA'} value={data&&data.product&&data.product[1] ?  data.product[1].count : 0} />,
 		                <Separator key={2123}/>,
-		                <GenericDisplayCardStrip key={'Zion'} label={'Zion'} value={'4'}/>,
+		                <GenericDisplayCardStrip key={'Zion'} label={'Zion'} value={data&&data.product&&data.product[2] ?  data.product[2].count : 0}/>,
 		                <Separator key={3123}/>,
-		                <GenericDisplayCardStrip key={'Flash LA'} label={'Flash LA'} value={'2'}/>
+		                <GenericDisplayCardStrip key={'Flash LA'} label={'Flash LA'} value={data&&data.product&&data.product[3] ?  data.product[3].count : 0}/>
 		                
               		]}
             	/>
@@ -99,8 +100,8 @@ const mapStateToProps = (state) => ({
   isConnected: state.network.isConnected,
   isVisible: state.common.isNetworkBannerVisible,
   currentScreen: state.common.currentScreen,
-  data     : state.insights.DashBoardSummaryData,
-  loader   : state.insights.loaders.getDashBoardSummaryLoader,
+  data: state.insights.DashboardSummaryData,
+  loader: state.insights.loaders.getDashboardSummaryLoader,
 })
 
 const mapDispatchToProps = (dispatch) => ({
