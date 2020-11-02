8ecaa12324bf7eea3b2dc4a2226699c9392340fa
Saurabh Gupta <saurabhsg83@gmail.com>
2020-11-02 16:09:29 +0530

    fixed warranty registered key issue

--- App/Containers/SchemeClaim/index.js
+++ App/Containers/SchemeClaim/index.js
@@ -92,7 +92,7 @@ class SchemeClaimInfoScreen extends Component {
                             <GenericDisplayCardStrip key={'Expected Claim Amount' + item.name} label={'Expected Claim Amount:'}  value={item.expected_claim_amount_by_dealer__c}  />,
                             <GenericDisplayCardStrip key={'Scheme Applicable' + item.name} label={'Scheme Applicable:'}  value={item.scheme_applicable_name}  />,
                             <GenericDisplayCardStrip key={'Customer Name' + item.name} label={'Customer Name:'}  value={item.customer_name__c}  />,
-                            <GenericDisplayCardStrip key={'Warranty Registered' + item.name} label={'Warranty Registered:'}  value={item.registered_for_warranty__c}  />,
+                            <GenericDisplayCardStrip key={'Warranty Registered' + item.name} label={'Warranty Registered:'}  value={item.registered_for_warranty__c  ? 'Yes' : 'No'}  />,
 
                         
                   ]}
